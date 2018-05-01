6ff72066cb06f0d353c34ab36e61af84a2dae724
Yuliia Khorkova <ykhorkova@gmail.com>
2018-05-01 00:10:50 +0000

    finished card

--- vanillajs-hello/src/js/index.js
+++ vanillajs-hello/src/js/index.js
@@ -10,6 +10,35 @@ import 'bootstrap';
 import '../style/index.scss';
 
 window.onload = function(){
-    console.log('Hello Rigo from the console!');
-    document.querySelector('.error').style.display = "none";
+    document.querySelector('.back-card').addEventListener('click',theButtonWasClicked);
 };
+
+function theButtonWasClicked(){
+    
+var rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "J", "Q", "K"];
+var types = ["&clubs;", "&diams;", "&hearts;", "&spades;"];
+var suits = ["clubs", "diams","hearts","spades"];
+var cards = new Array();
+    
+    var randomType = Math.floor(Math.random() *3);
+    var randomRank = Math.floor(Math.random() *13);
+
+    let theSymbol = document.querySelector('.symbol');
+    theSymbol.innerHTML = "<span>"+types[randomType]+"</span>";
+    
+    let theUpperNumber = document.querySelector('.upper-number');
+    theUpperNumber.innerHTML = "<span>"+rank[randomRank]+"</span>";
+    
+    let theLowerNumber = document.querySelector('.lower-number');
+    theLowerNumber.innerHTML = "<span>"+rank[randomRank]+"</span>";
+
+    let theCard = document.querySelector('.card');
+    theCard .className = "card "+suits[randomType];
+}
+
+
+
+//     function newStyles(){
+//     document.querySelector('.hearts').style.color = 'red';
+// }
